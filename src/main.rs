42dc44db3410e84ab7835819a07ea77649be02eb
EC2 Default User <robert-snakard@github.com>
2019-02-19 22:09:29 +0000

    parse guess into int

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,10 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!(),
+    };
+
     println!("You guessed: {}", guess);
 }
